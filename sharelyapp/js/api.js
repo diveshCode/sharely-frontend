0d195a61b24bbe932865e65af5d59a4fc5f3ebd2
Divesh Choudhry <diveshchoudhryjd@gmail.com>
2026-03-17 20:42:37 +0530

    fix navbar

--- sharelyapp/js/api.js
+++ sharelyapp/js/api.js
@@ -3,4 +3,4 @@ const base = "https://res.cloudinary.com/dxqxq0pkc/";
 
 // const API = "http://127.0.0.1:8000/api";
 const token = localStorage.getItem("access");
-const API = "https://shareit-42a7.onrender.com/api";
+const API = "https://shareit-42a7.onrender.com/api"
